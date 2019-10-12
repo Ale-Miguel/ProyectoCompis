ae2928c60bf75ca0e0d49c8ac247ecbd4beb2dba
Ale-Miguel <alemiguel97@hotmail.com>
2019-10-12 01:53:48 -0500

    Parser y scanner  reconocen bien el lenguaje

---
+++ ProyectoPruebas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProyectoPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Scanner scanner = new Scanner("Programa.txt");
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            Console.WriteLine(parser.errors.count + " errors found");
+            //Console.WriteLine(parser.errors.errMsgFormat);
+        }
+    }
+}
